c4969319c9f7c1100f35c6cfc662093e5d06bb1f
zhuleiming03@gmail.com <zhuleiming03@gmail.com>
2020-06-13 15:02:45 +0800

    init

---
+++ maven/hutool/src/main/java/hutool/convert/DigitToChineseTest.java
@@ -0,0 +1,23 @@
+package hutool.convert;
+
+import cn.hutool.core.convert.Convert;
+
+public class DigitToChineseTest {
+
+    public static void main(String[] args) {
+        double money = 12345678.199995;
+        System.out.printf("%s : %s \n", money, Convert.digitToChinese(money));
+
+        money = -199.01;
+        System.out.printf("%s : %s \n", money, Convert.digitToChinese(money));
+
+        money = -0.219;
+        System.out.printf("%s : %s \n", money, Convert.digitToChinese(money));
+
+        money = 0.00;
+        System.out.printf("%s : %s \n", money, Convert.digitToChinese(money));
+
+        money = 199.00;
+        System.out.printf("%s : %s \n", money, Convert.digitToChinese(money));
+    }
+}
