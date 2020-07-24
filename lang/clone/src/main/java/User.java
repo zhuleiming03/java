0d893df50d36f2ab578ee9c4340c3299769d59a0
lmzhu <zhuleiming03@goolge.com>
2020-07-24 10:51:26 +0800

    add

---
+++ lang/clone/src/main/java/User.java
@@ -0,0 +1,40 @@
+public class User implements Cloneable {
+
+    private Integer id;
+
+    private String name;
+
+    @Override
+    public User clone() throws CloneNotSupportedException {
+        return (User) super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
