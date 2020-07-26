bf6953a10fc89f408362b4174ee94747044f4854
zhuleiming <zhuleiming03@gmail.com>
2020-07-26 14:48:50 +0800

    init

---
+++ maven/cglib/src/main/java/pojo/Dog.java
@@ -0,0 +1,14 @@
+package pojo;
+
+public class Dog implements Animal {
+
+    @Override
+    public void run() {
+        System.out.println("dog is running");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("dog eating meat");
+    }
+}
---
+++ maven/cglib/src/test/java/JdkAgentTest.java
@@ -0,0 +1,43 @@
+import pojo.*;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class JdkAgentTest {
+
+    /**
+     * jdk 静态代理
+     */
+    @Test
+    public void staticAgent() {
+        DogAgent agent = new DogAgent(new Dog());
+        agent.run();
+        agent.eat();
+    }
+
+    /**
+     * jdk 动态代理
+     */
+    @Test
+    public void dynamicAgent() {
+        Animal dog = new Dog();
+        Class<?> dogClass = dog.getClass();
+
+        Animal proxyInstance = (Animal) Proxy.newProxyInstance(
+                dogClass.getClassLoader(),
+                dogClass.getInterfaces(),
+                new InvocationHandler() {
+                    // 重写代理方法
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println(">> dog's master is Tom");
+                        method.invoke(dog, args);
+                        return null;
+                    }
+                });
+        proxyInstance.run();
+        proxyInstance.eat();
+    }
+}
