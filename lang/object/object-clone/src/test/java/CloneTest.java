7687177c10076c564eebbf75c25c21e046dc3e69
zhuleiming <zhuleiming03@gmail.com>
2020-07-26 14:03:46 +0800

    init

---
+++ lang/object/object-clone/src/test/java/CloneTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import pojo.User;
+
+public class CloneTest {
+
+    @Test
+    public  void ObjectClone() {
+
+        try {
+            System.out.println(" Sean 是 Tom 的克隆体");
+            User Tom = new User(1, "Tom");
+            User Sean = Tom.clone();
+            System.out.println("Tom :" + Tom);
+            System.out.println("Sean :" + Sean);
+            System.out.println(" Tom 修改属性 id");
+            Tom.setId(2);
+            System.out.println("Tom :" + Tom);
+            System.out.println("Sean :" + Sean);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
