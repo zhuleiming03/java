152d0f93b976355927243fb8b77a0092f149f4b4
lmzhu <zhuleiming03@goolge.com>
2020-12-07 14:16:04 +0800

    alter

--- util/map/src/test/java/StreamTest.java
+++ util/map/src/test/java/StreamTest.java
@@ -1,13 +1,12 @@
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamTest {
 
     @Test
-    public void baseTest() {
+    public void joiningTest() {
         Map<Integer, String> hashMap = new HashMap<>();
         hashMap.put(1, "one");
         hashMap.put(3, "one");
@@ -15,5 +14,17 @@ public class StreamTest {
 
         System.out.println(hashMap.values().stream().distinct().collect(Collectors.joining(";")));
 
+    }
+
+    @Test
+    public void flatMapTest() {
+        Map<Integer, List<String>> haspMap = new HashMap<>();
+        haspMap.put(1, Arrays.asList("1", "one"));
+        haspMap.put(2, Collections.singletonList("two"));
+        haspMap.put(3, Arrays.asList("No.3", "three"));
+
+        haspMap.values().stream()
+                .flatMap(Collection::stream)
+                .forEach(System.out::println);
     }
 }
